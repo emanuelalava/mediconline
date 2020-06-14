9a25d89138d90624abf7e6c3d1880d272c814fc5
Winter Alava <walava@espol.edu.ec>
2020-06-14 17:23:10 -0500

    Programando el SplashActivity.java para que se muestre durante 3 seg

--- app/src/main/java/com/example/mediconline/SplashActivity.java
+++ app/src/main/java/com/example/mediconline/SplashActivity.java
@@ -2,13 +2,25 @@ package com.example.mediconline;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashActivity extends AppCompatActivity {
-
+    private final int msDuration = 3000; // 3 segundos
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+                // Cuando pasen los 3 segundos, pasamos a la actividad principal de la aplicación
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+
+            ;
+        }, msDuration);
     }
 }
